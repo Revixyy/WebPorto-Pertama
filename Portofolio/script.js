4f5e24b494d8936a2010f44c11d18ed9c0ba7ccf
Revixyy <146611739+Revixyy@users.noreply.github.com>
2023-10-05 18:00:58 +0700

    Add files via upload

---
+++ Portofolio/script.js
@@ -0,0 +1,8 @@
+let text = document.querySelector(".text");
+var typed = new Typed(text, {
+    strings: ["Full-Stack Developer", "Entrepreneur", "Software Engineer", "Gamers"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    backDelay: 1000,
+    loop: true,
+});
